8985d01769c4ac5fc778fc639716ce06f19e8ad1
sootn <encrypt.wave@gmail.com>
2015-02-25 15:40:36 +0100

    Every mod 100

--- recalleachiter.m
+++ recalleachiter.m
@@ -3,11 +3,16 @@ function [ Pattern ] = recalleachiter( Pattern, Wights)
 %   Detailed explanation goes here
     
     [IGNORE, N] = size(Pattern);
+    
     while true
+        k = 0;
         for i = randperm(N)
             Pattern(i) = sgn(Pattern*Wights(:, i));
+            k = k + 1;
+            if mod(k, 100) == 0
+                pause(4);
+                vis(Pattern);
+            end
         end
-        pause(0);
-        vis(Pattern);
     end
 end
